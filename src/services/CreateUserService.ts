99a728877b2d309c2b4b5f96ff553abbed1b6f0c
MorenaNobre <morenagnobre@gmail.com>
2021-09-26 12:34:33 -0300

    created Services Layer

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,37 @@
+//Aqui terá tudo que for relacionado a criação de usuário
+
+import { UsersRepositories } from "../repositories/UsersRepositories";
+
+interface IUseRequest {
+  name: string;
+  email: string;
+  admin?: boolean;
+}
+
+class CreateUserService {
+  async execute({ name, email, admin }: IUseRequest) {
+    const usersRepository = new UsersRepositories();
+
+    if (!email) {
+      throw new Error("Email incorrect");
+    }
+
+    const userAlreadyExists = await usersRepository.findOne({ email });
+
+    if (userAlreadyExists) {
+      throw new Error("User already exists");
+    }
+
+    const user = usersRepository.create({
+      name,
+      email,
+      admin,
+    });
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export { CreateUserService };
