3ff98a4acfd2b21d3c1fd43c9f8c408b913e058a
MorenaNobre <morenagnobre@gmail.com>
2021-10-02 09:14:00 -0300

    criação de Middleware para verificação de usuário adm/ ou não

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { CreateUserController } from "./controllers/CreateUserController";
 import { CreateBookController } from "./controllers/CreateBookController";
+import { ensureAdmin } from "./middlewares/ensureAdmin";
 
 const router = Router();
 
@@ -8,6 +9,6 @@ const createUserController = new CreateUserController();
 const createBookController = new CreateBookController();
 
 router.post("/users", createUserController.handle);
-router.post("/books", createBookController.handle);
+router.post("/books", ensureAdmin, createBookController.handle);
 
 export { router };
